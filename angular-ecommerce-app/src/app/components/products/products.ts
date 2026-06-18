405f7f561a01b5d00b24cdab96494f8e1c3ef911
tamilselvan <tamilselvan.arumugam.a@gmail.com>
2026-06-18 13:13:23 +0530

    Amazon Clone Final Update

---
+++ angular-ecommerce-app/src/app/components/products/products.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone 15 Pro',
+    category: 'Mobiles',
+    price: 129999,
+    image: 'mobile1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Samsung S24',
+    category: 'Mobiles',
+    price: 89999,
+    image: 'mobile2.jpg'
+  },
+  {
+    id: 3,
+    name: 'OnePlus 13',
+    category: 'Mobiles',
+    price: 69999,
+    image: 'mobile3.jpg'
+  },
+  {
+    id: 4,
+    name: 'MacBook Air M4',
+    category: 'Laptops',
+    price: 109999,
+    image: 'laptop1.jpg'
+  },
+  {
+    id: 5,
+    name: 'Dell XPS',
+    category: 'Laptops',
+    price: 89999,
+    image: 'laptop2.jpg'
+  }
+];
